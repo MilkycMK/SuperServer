f4e856a9b2ece7beaac6a1e980f16fd333b0fbfe
milkyc <sagaidaan@yandex.ru>
2023-10-25 13:36:15 +0300

    todo database initialization

---
+++ src/main/java/net/mlk/adolfserver/data/todo/TodoElement.java
@@ -0,0 +1,93 @@
+package net.mlk.adolfserver.data.todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todo")
+public class TodoElement {
+    @Id
+    private int id;
+    private String name;
+    private String header;
+    private String description;
+    private String files;
+    private LocalDateTime creation_time;
+    private LocalDateTime task_time;
+
+    protected TodoElement() {
+
+    }
+
+    public TodoElement(String name, String header, LocalDateTime creation_time) {
+        this(name, header, null, creation_time);
+    }
+
+    public TodoElement(String name, String header, String description, LocalDateTime creation_time) {
+        this(name, header, description, null, creation_time);
+    }
+
+    public TodoElement(String name, String header, String description, String files, LocalDateTime creation_time) {
+        this(name, header, description, files, creation_time, null);
+    }
+
+    public TodoElement(String name, String header, String description, String files, LocalDateTime creation_time, LocalDateTime task_time) {
+        this.setName(name);
+        this.setHeader(header);
+        this.setDescription(description);
+        this.setFiles(files);
+        this.setCreationTime(creation_time);
+        this.setTaskTime(task_time);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getHeader() {
+        return this.header;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFiles(String files) {
+        this.files = files;
+    }
+
+    public String getFiles() {
+        return this.files;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creation_time = creationTime;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return this.creation_time;
+    }
+
+    public void setTaskTime(LocalDateTime taskTime) {
+        this.task_time = taskTime;
+    }
+
+    public LocalDateTime getTaskTime() {
+        return this.task_time;
+    }
+}
---
+++ src/main/java/net/mlk/adolfserver/data/todo/TodoService.java
@@ -0,0 +1,22 @@
+package net.mlk.adolfserver.data.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("TodoService")
+public class TodoService {
+    private static TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        TodoService.todoRepository = todoRepository;
+    }
+
+    public static void saveTodo(TodoElement element) {
+        todoRepository.save(element);
+    }
+
+    public static TodoRepository getTodoRepository() {
+        return todoRepository;
+    }
+}
