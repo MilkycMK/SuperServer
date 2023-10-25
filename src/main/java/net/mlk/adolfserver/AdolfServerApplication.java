e490d9fc1a824e8e50fe97bc0cbecc25776155c2
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-25 18:08:43 +0300

    basic files

--- src/main/java/net/mlk/adolfserver/AdolfServerApplication.java
+++ src/main/java/net/mlk/adolfserver/AdolfServerApplication.java
@@ -2,12 +2,21 @@ package net.mlk.adolfserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 @SpringBootApplication
 public class AdolfServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdolfServerApplication.class, args);
+    }
+
+    @RequestMapping(path = {"", "/"})
+    public String index() {
+        return "test";
     }
 
 }
