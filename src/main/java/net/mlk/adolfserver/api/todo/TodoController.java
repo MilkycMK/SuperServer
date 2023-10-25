6767852a2a0a1acf2928556c0d245a13b8b15f0f
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-25 18:08:43 +0300

    file save example

--- src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
+++ src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
@@ -9,8 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.File;
+import org.springframework.web.multipart.MultipartFile;
 //        AdolfServerApplication.FORMAT
 
 @Controller
@@ -24,11 +23,17 @@ public class TodoController {
                                              @RequestParam(value = "creation_time", required = false) String creationTime,
                                              @RequestParam(value = "task_time", required = false) String taskTime,
                                              @RequestParam(value = "description", required = false) String description,
-                                             @RequestParam(value = "files", required = false) File[] files,
+                                             @RequestParam(value = "files", required = false) MultipartFile[] files,
                                              HttpServletRequest request,
                                              HttpServletResponse response) {
         Session session = (Session) request.getAttribute("session");
-        System.out.println(header);
+//        System.out.println(header);
+//        System.out.println(description);
+//        System.out.println(Arrays.toString(files));
+//        for (MultipartFile file : files) {
+//            FileOutputStream stream = new FileOutputStream(file.getOriginalFilename());
+//            stream.write(file.getBytes());
+//        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
