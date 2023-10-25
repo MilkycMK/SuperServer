e168e203b0aea7d01f4ea0b05d1ea00d60ac0ac1
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-25 20:13:03 +0300

    added file checking

--- src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
+++ src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -22,7 +22,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@ControllerAdvice
 public class TodoController {
+    public static final int MAX_FILE_SIZE = 10; // mb
 
     @PostMapping(path = {"/todo", "/todo/"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> todoCreate(@RequestParam(value = "header") String header,
@@ -33,13 +35,16 @@ public class TodoController {
                                              HttpServletResponse response) throws IOException {
 
         Session session = (Session) request.getAttribute("session");
-
         String name = session.getName();
 
         if (header.isEmpty()) {
             return new ResponseEntity<>(new ResponseError("Заголовок пустует...").toString(), HttpStatus.BAD_REQUEST);
-        } else if (!compareFormat(taskTime, AdolfServerApplication.FORMAT)) {
+        } else if (taskTime != null && !compareFormat(taskTime, AdolfServerApplication.FORMAT)) {
             return new ResponseEntity<>(new ResponseError("Неверный формат времени.").toString(), HttpStatus.BAD_REQUEST);
+        } else if (files != null) {
+            if (files.length > 5) {
+                return new ResponseEntity<>(new ResponseError("Максимум можно загрузить 5 файлов.").toString(), HttpStatus.BAD_REQUEST);
+            }
         }
 
         List<String> fileNames = new ArrayList<>();
@@ -63,13 +68,20 @@ public class TodoController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    public static boolean compareFormat(String inputValue, DateTimeFormatter format) {
+    private static boolean compareFormat(String inputValue, DateTimeFormatter format) {
         try {
             format.parse(inputValue);
             return true;
         } catch (DateTimeParseException dtpe) {
             return false;
         }
+    }
+
+
+    @ExceptionHandler(MultipartException.class)
+    @ResponseBody
+    public ResponseEntity<String> multipartExceptionHandler(MultipartException e) {
+        return new ResponseEntity<>(new ResponseError("Размер файлов не должен быть больше " + MAX_FILE_SIZE + "мб").toString(), HttpStatus.BAD_REQUEST);
     }
 
 }
