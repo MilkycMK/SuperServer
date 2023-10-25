602d8b162ff334e54e4b0aeb8e6c71edc23a0f1a
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-25 18:08:43 +0300

    basic todo creation

--- src/main/java/net/mlk/adolfserver/AdolfServerApplication.java
+++ src/main/java/net/mlk/adolfserver/AdolfServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
 import java.time.format.DateTimeFormatter;
 
 @Controller
@@ -12,6 +14,10 @@ public class AdolfServerApplication {
     public static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
+        File file = new File("userFiles");
+        if (!file.exists()) {
+            file.mkdir();
+        }
         SpringApplication.run(AdolfServerApplication.class, args);
     }
 
--- src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
+++ src/main/java/net/mlk/adolfserver/api/todo/TodoController.java
@@ -2,7 +2,9 @@ package net.mlk.adolfserver.api.todo;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.mlk.adolfserver.AdolfServerApplication;
 import net.mlk.adolfserver.data.session.Session;
+import net.mlk.adolfserver.data.todo.TodoElement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,30 +12,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-//        AdolfServerApplication.FORMAT
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TodoController {
 
-    /*
-        files - для отправки необходим тип multipart потом напишу пример для фронта
-     */
     @PostMapping(path = {"/todo", "/todo/"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> todoCreate(@RequestParam(value = "header", required = false) String header,
-                                             @RequestParam(value = "creation_time", required = false) String creationTime,
+    public ResponseEntity<String> todoCreate(@RequestParam(value = "header") String header,
                                              @RequestParam(value = "task_time", required = false) String taskTime,
                                              @RequestParam(value = "description", required = false) String description,
                                              @RequestParam(value = "files", required = false) MultipartFile[] files,
                                              HttpServletRequest request,
-                                             HttpServletResponse response) {
+                                             HttpServletResponse response) throws IOException {
         Session session = (Session) request.getAttribute("session");
-//        System.out.println(header);
-//        System.out.println(description);
-//        System.out.println(Arrays.toString(files));
-//        for (MultipartFile file : files) {
-//            FileOutputStream stream = new FileOutputStream(file.getOriginalFilename());
-//            stream.write(file.getBytes());
-//        }
+        String name = session.getName();
+        List<String> fileNames = new ArrayList<>();
+        System.out.println(Arrays.toString(files));
+        if (files != null) {
+            for (MultipartFile file : files) {
+                String fileName = file.getOriginalFilename();
+                fileNames.add(fileName);
+                try (OutputStream outStream = new FileOutputStream("userFiles/" + name + "/" + fileName)) {
+                    outStream.write(file.getBytes());
+                }
+            }
+        }
+
+        LocalDateTime created = LocalDateTime.now();
+        LocalDateTime tasked = null;
+        if (taskTime != null) {
+            tasked = LocalDateTime.parse(taskTime, AdolfServerApplication.FORMAT);
+        }
+        TodoElement element = new TodoElement(name, header, description, fileNames, created, tasked);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
