c14e378d09e43e4518600b513e42096d62b01404
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-27 14:51:20 +0300

    a little request

--- src/main/java/net/mlk/adolfserver/data/todo/files/UserFileRepository.java
+++ src/main/java/net/mlk/adolfserver/data/todo/files/UserFileRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserFileRepository extends JpaRepository<UserFile, Integer> {
+    UserFile findByUserNameAndTaskIdAndFileName(String name, int taskId, String fileName);
 }
