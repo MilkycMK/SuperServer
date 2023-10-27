7d2e72b3ca58e9240f18566aec2e158dad20ba2b
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-27 13:03:40 +0300

    remove IgnoreCase condition

--- src/main/java/net/mlk/adolfserver/data/user/UserRepository.java
+++ src/main/java/net/mlk/adolfserver/data/user/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean existsByNameIgnoreCase(String name);
-    User findByNameIgnoreCase(String name);
+    boolean existsByName(String name);
+    User findByName(String name);
 }
