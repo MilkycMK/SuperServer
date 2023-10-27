054b1cf65608d3914b7e9f0efa01f059a21e1aca
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-27 13:02:40 +0300

    file database structure update

--- src/main/java/net/mlk/adolfserver/data/todo/TodoRepository.java
+++ src/main/java/net/mlk/adolfserver/data/todo/TodoRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoElement, Integer> {
-    TodoElement findByIdAndNameIgnoreCase(int id, String name);
-    @Query(value = "SELECT task_time FROM todo WHERE name=?1", nativeQuery = true)
-    JsonList findAllDatesByNameIgnoreCase(String name);
-    JsonList findAllByNameIgnoreCaseAndTaskTime(String name, LocalDateTime time);
+    TodoElement findByIdAndUserName(int id, String name);
+    @Query(value = "SELECT CONVERT(task_time, DATE) FROM todo WHERE user_name=?1", nativeQuery = true)
+    JsonList findAllDatesByName(String name);
+    @Query(value = "SELECT * FROM todo WHERE user_name=?1 AND DATE(task_time)=?2", nativeQuery = true)
+    List<TodoElement> findAllByUserNameAndTaskTime(String name, LocalDate time);
 }
