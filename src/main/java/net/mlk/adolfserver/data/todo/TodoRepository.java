97e65569d3ae12a445088003d6e70223d8294ad6
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-28 11:33:49 +0300

    change username to id

--- src/main/java/net/mlk/adolfserver/data/todo/TodoRepository.java
+++ src/main/java/net/mlk/adolfserver/data/todo/TodoRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoElement, Integer> {
-    TodoElement findByIdAndUserName(int id, String name);
-    @Query(value = "SELECT CONVERT(task_time, DATE) FROM todo WHERE user_name=?1", nativeQuery = true)
-    JsonList findAllDatesByName(String name);
-    @Query(value = "SELECT * FROM todo WHERE user_name=?1 AND DATE(task_time)=?2", nativeQuery = true)
-    List<TodoElement> findAllByUserNameAndTaskTime(String name, LocalDate time);
+    TodoElement findByIdAndUserId(int id, int userId);
+    @Query(value = "SELECT DATE(task_time) FROM todo WHERE user_id=?1", nativeQuery = true)
+    JsonList findAllDatesByUserId(int id);
+    @Query(value = "SELECT * FROM todo WHERE user_id=?1 AND DATE(task_time)=?2", nativeQuery = true)
+    List<TodoElement> findAllByUserIdAndTaskTime(int userId, LocalDate time);
 }
