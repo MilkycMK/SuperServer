8a31f3be92114a001b0c62479cadb30dd286ba34
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-29 13:24:09 +0300

    deleted for remake

--- src/main/java/net/mlk/adolfserver/data/todo/files/UserFileRepository.java
+++
@@ -1,9 +0,0 @@
-package net.mlk.adolfserver.data.todo.files;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserFileRepository extends JpaRepository<UserFile, Integer> {
-    UserFile findByUserIdAndTaskIdAndFileName(int id, int taskId, String fileName);
-}
