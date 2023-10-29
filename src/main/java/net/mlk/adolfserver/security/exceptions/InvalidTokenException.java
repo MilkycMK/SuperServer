34a866d2af711da953d6305b9ec78c2ac40bd7e3
Milkyc <123307475+MilkycMK@users.noreply.github.com>
2023-10-29 16:34:10 +0300

    removed unused exception

--- src/main/java/net/mlk/adolfserver/security/exceptions/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package net.mlk.adolfserver.security.exceptions;
-
-public class InvalidTokenException extends Exception {
-
-    public InvalidTokenException() {
-        super("User token is invalid");
-    }
-
-}
